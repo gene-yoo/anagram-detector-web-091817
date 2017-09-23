db9045affdbe4b0739533a1ec498c935afb30bfd
gene-yoo <gene.y.yoo@gmail.com>
2017-09-23 12:09:08 -0400

    Create Anagram class and instance method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def match(words)
+    words.select do |word|
+      word.split("").sort == @text.split("").sort
+    end
+  end
+end
